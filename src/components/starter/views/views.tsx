89da7b0809a3212dd593ac201ec98f731cf86215
fahd bouacherine <fahdboua@gmail.com>
2023-12-22 15:40:31 +0000

    commit add multiple words

--- src/components/starter/views/views.tsx
+++ src/components/starter/views/views.tsx
@@ -7,11 +7,19 @@ export default component$(() => {
   const loading = useSignal(false);
   const error = useSignal<string>('');
   const hashtags = useStore<string[]>([]);
+  const words = useStore<string[]>([]);
+
+  const handleClick = $(() => {
+    const inputElement = document.getElementById("inputWords") as HTMLInputElement | null;
+    if (inputElement != null && !words.includes(inputElement.value)) {
+      words.push(inputElement.value);
+    }
+  });
 
   const fetchAndUpdateHashtags = $(async (word: string,loading: Signal<boolean>, error: Signal<string>) => {
     try {
       loading.value = true;
-  
+
       const fetchedHashtags: string[] = await fetchHashtags(word);
   
       hashtags.length = 0;
@@ -33,6 +41,7 @@ export default component$(() => {
       <div class={styles.inputContainer}>
         <input
           type="text"
+          id='inputWords'
           class={styles.wordInput}
           placeholder="Enter a word"
           onKeyUp$={async (event: KeyboardEvent) => {
@@ -41,12 +50,23 @@ export default component$(() => {
             }
           }}
         />
-        <button class={styles.iconButton} onClick$={() => console.log("add")}>
+        <button class={styles.iconButton} onClick$={() => handleClick()}>
           <i class={`fa-solid fa-plus`} />
         </button>
         <button class={styles.iconButton} onClick$={() => fetchAndUpdateHashtags((document.querySelector(`.${styles.wordInput}`) as HTMLInputElement).value,loading,error)}>
           <i class={`fa-solid fa-magnifying-glass`} />
         </button>
+      </div>
+      <div class={styles.words}>
+        {words && words.map((word, index) => (
+          <button
+            key={index}
+            class={styles.iconButton}
+          >
+            {word}
+            <i class={`fa-solid fa-xmark`} />
+          </button>
+        ))}
       </div>
       <div>
         <textarea class={styles.myTextarea} value={hashtags.join("\n")} />
